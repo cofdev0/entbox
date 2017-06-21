f2bd4c16757fc79ada8168478512adf286b1424f
cofdev0 <cofdev0@hh2013.33mail.com>
2017-06-21 14:44:34 +0200

    chat with websocket

--- entboxapp/index.android.js
+++ entboxapp/index.android.js
@@ -9,26 +9,78 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+    TouchableOpacity,
+    TextInput
 } from 'react-native';
 
+// import {Server as PeerUpServer}  from 'peer-up/dist/index';
+// const fs = require('fs-extra');
+//
+// const server:PeerUpServer = new PeerUpServer();
+// server.listen();
+
 export default class entboxapp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            connected: false,
+            msg:""
+        };
+        this.socket = new WebSocket('ws://45.32.186.169:28475');
+        this.socket.onopen = () => {
+            this.setState({connected:true})
+        };
+        this.socket.onmessage = (e) => {
+            console.log(e.data);
+        };
+        this.socket.onerror = (e) => {
+            console.log(e.message);
+        };
+        this.socket.onclose = (e) => {
+            this.setState({connected:false})
+            console.log(e.code, e.reason);
+        };
+        this.emit = this.emit.bind(this);
+
+    }
+
+    emit() {
+        if( this.state.connected ) {
+            this.socket.send(this.state.msg)
+        }
+    }
+
+    onMsgChange(event) {
+        this.setState({msg:event.target.value});
+    }
+
+    render() {
+        return (
+            <View style = {styles.container}>
+                <TextInput style = {styles.input}
+                           underlineColorAndroid = "transparent"
+                           placeholder = "chat msg"
+                           placeholderTextColor = "#9a73ef"
+                           autoCapitalize = "none"
+                           onChangeText = {(text)=>{this.setState({msg:text})}}
+                           value={this.state.msg}
+                />
+
+                <TouchableOpacity
+                    style = {styles.submitButton}
+                    onPress = { ()=> this.emit()}>
+                    <Text style = {styles.submitButtonText}>
+                        Submit
+                    </Text>
+                </TouchableOpacity>
+                    <Text>connected:{this.state.connected ? "true":"false"}</Text>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
